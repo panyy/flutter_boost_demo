29d7cc9a146be8ec569b3f553e0a48db926c406f
liushuxin <admin@example.com>
2025-01-22 22:01:25 +0800

    feat: update ios demo

---
+++ ios/demo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by apple on 2025/1/22.
+//
+
+#import <UIKit/UIKit.h>
+#import <Flutter/Flutter.h>
+
+@interface AppDelegate : FlutterAppDelegate
+
+@end
+
